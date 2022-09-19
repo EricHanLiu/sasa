13634a3f5629411526b0b5074fb15c9db5615eaf
Eric Liu <erichanliu@google.com>
2022-09-19 11:13:44 -0700

    Image styling and JS populating content

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("test");
+let parent = document.querySelector('#parent');
+let leftTemplate = document.querySelector('#left-template');
+let rightTemplate = document.querySelector('#right-template');
+
+for (let i = 0; i < 10; i++) {
+    let l = leftTemplate.cloneNode(true);
+    let r = rightTemplate.cloneNode(true);
+    l.classList.remove('is-hidden');
+    r.classList.remove('is-hidden');
+    parent.appendChild(l);
+    parent.appendChild(r);
+}
