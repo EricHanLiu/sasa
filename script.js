e2e3ec3bfa9a34f749ebb7a1d7fa256205093b9e
Eric Liu <erichanliu@google.com>
2022-09-30 11:59:51 -0700

    Add all images

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let parent = document.querySelector('#parent');
 let leftTemplate = document.querySelector('#left-template');
 let rightTemplate = document.querySelector('#right-template');
+let numImages = 50;
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < numImages - 1; i += 2) {
     let l = leftTemplate.cloneNode(true);
     let r = rightTemplate.cloneNode(true);
     l.classList.remove('is-hidden');
     r.classList.remove('is-hidden');
+
+    // Set the image and text.
+    l.querySelector('img').src = `images/img${i}.jpg`;
+    r.querySelector('img').src = `images/img${i + 1}.jpg`;
+
+    console.log(l.querySelector('img').src);
+    console.log(r.querySelector('img').src);
+
     parent.appendChild(l);
     parent.appendChild(r);
 }
