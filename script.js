224a629b9ab91b6acec5dd916b6117315b4d585e
Eric Liu <erichanliu@google.com>
2022-10-03 02:05:31 -0700

    Add first half of text entries

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+let text = `Oct 13, 2021; Your 24th birthday, and a great throwback to our (sometimes tumultuous) times at Carlos Sr's. This is where our love stood its first true test and thankfully we made it out largely unscathed. This was a great peek into how caring you are as a person, and the lengths you go for the people you love ❤️. Also shoutout the surprise gift from Iz 🍩!
+Oct 24, 2021; The really random and super fun party in SF! You look absolutely adorable and this was overall a super fun night turning up with SAME. You got to see a glimpse of my true klepto side 😏
+Oct 27, 2021; Our one Ramen Nagi experience! There are very few things I enjoy more than eating delicious food with you 🍜
+Oct 30, 2021; OKAY MAEVE WILLIAMS!!! You looked hot AF and got the pose perfect. Sadly your costume was outshined (literally) by my golden opulence 👑
+Nov 4, 2021; Our favorite restaurant in Redwood City. You honestly look so fkin adorable I can'tttt (nice jacket too 👀). We should go back here soon, although will it ever be the same now that you're vegetarian? The honey sausage is the move... 
+Nov 4, 2021 (cont.); The very short lived attempt at you teaching me how to sew (had to lookup how to spell this lol). You look very Russian here
+Nov 14, 2021; YES SUR. What a beautiful place and amazing weekend. Particularly enjoyed the ice cream in that really cool cafe by the water, camping the previous night, seeing DOLPHINS, wrecking your car and ziptying it back together, and Vince randomly being there lol
+Nov 14, 2021 (cont.); My first and only Terun experience, food was good but price was wack. Just a cute picture of you, mid experience overall xD
+Dec 2, 2021; HANGING OUT WITH WENDY AND JASON (RIP 😭). Playing cards (that left hand of the red baron or whatever game, weird) and delicious mapo tofu I think?? Fun night overall and cute pic of the four of us 🥹. Nice jacket
+Dec 7, 2021; SHOUTOUT GEO! Putting us up with the sickest holiday event, delicious free sliders and drinks and watching Encanto together ❤️. Nice jacket (also our plates look a little excessive)
+Dec 25, 2021; Merry Christmas 🎄!!! It's difficult to summarize this entire stay in Boston with few enough words such that the text box doesn't overflow the image, but en bref: being with your family and feeling the warmth of your home truly meant the world to me, and gave me so much happiness. It reminded me of the few Christmases I got to experience with my mom and dad back in Toronto, and made me feel loved and included and a part of something. I deeply missed it as soon as I left. Thank you so much for having me in your home and letting me experience the holidays with the love of your family ❤️. Some highlights: all the delicious food Mila made, playing tres patas and the other aguinaldos (damn am I fluent?), coming up with all the gifts (real and joke), the walks along the Charles, and every minute I got to spend with you in your home. I miss Boston! Let's go back soon 😊
+Jan 9, 2022; PINNACLES 🏔. What a great trip with an even greater crew (I really liked Elijah, tough). We successfully mounted the long and hard shaft of mountain without even breaking a sweat, went caving, used those railing things, and had great chats throughout this 8 hour hiking experience. Plus you look adorable in a cap 🥰
+Jan 10, 2022; I don't even remember what movie we watched here, but shoutout this great theatre with delicious popcorn. Damn my hair looked good here 🥵
+Jan 31, 2022; WENDY'S CHINESE NEW YEAR'S PARTY 🇨🇳, we absolutely stunted on everyone there and ate some delicious mapo tofu and dumplings. Gonna miss Wendy and Jason hosting these extravagant parties, the high life suits her. We also called my dad later this night, and it was really sweet of you to stay on the call with us 😊. I love you so much
+Feb 12, 2022; Zazie with SAME! Really enjoyed eating $20 tiny portion breakfast (oh but tip was included!!!xD1!11!!) and your beautiful dead eyes 💓
+Feb 14, 2022; VALENTINE'S DAY 💘, what an amazing day overall this was even though we almost broke up for some reason I can no longer remember xD. The delicious Alison Roman pasta and your amazing slutty fashion sense (I really like that shirt btw). I also don't know what that pose is but keep killing it sas 👌
+Feb 17, 2022; Trying the Pho place by my house and some photo-ception. Honestly the food was pretty mid but the experience was a ten, and any time we get to eat together I'm happy. Nice jacket
+Feb 19, 2022; Geraldine's birthday! This was honestly a very fun day even though we started hella late and it got dark quick. We look really cute in this photo and I miss that group, we've had some great times together 🫂. Dinner at your place after was amazing, cooking the SUMMER PASTA and drinking and chatting with everyone. Also I filmed you brushing your teeth for some reason, you must've been wasted
+Mar 4, 2022; YOSEMITE WEEKEND. One of the highlights right off the bat was our 4 our drive up together, McDonalds breakfast a classic and getting to spend the first day just the two of us and explore the valley was such an amazing experience
+Mar 4, 2022 (cont.); Seeing the falls and doing our wrong-direction loop hike was honestly a highlight of the weekend, not to mention the shenanigans we pulled off in our AirBnB later that night 👀😏. I love you so much!
+Mar 5, 2022; Second day of Yosemite with Aaron and Maddy! Making snowmen in the field, seeing and exploring Lower Yosemite Falls, and our attempt at hiking up Yosemite falls getting thwarted by the weather. But we still got some amazing views in and a wonderful weekend overall
+Mar 16, 2022; SHIZEN OMFG. Probably my favorite dining experience together in SF, but not sure if that was cause of the food or because of how smokin hot 🚭 you looked. I'll literally come eat here any time you want (I think I might owe you a Shizen dinner anyway??), so just hit me up whenever girl
+Apr 3, 2022; Golden Gate Park! You looking beautiful under this cherry blossom tree, us picnicing by the Conservatory, walking and talking together throughout the park (one of my favorite activities to do together despite my general dislike of walking... should tell you how much I enjoy our talking 😉). I still need to bring my bike up so we can bike through the park together!
+Apr 9, 2022; Wine tasting with Rains! Eating delicious fudge, drinking delicious 18% sugary port, you looking GORGEOUS in Nancy's dress, and a beautiful sunny day. Could life get any better??
+Apr 9, 2022 (cont.); Yeah, that's my girlfriend. She's fine af 🔥
+Jun 1, 2022; Well it appears I skipped two key events when selecting these photos: the Lizzo concert on Apr 19 (lmao what an experience), and our first surfing experience together on May 22! Pretty big time skip here, I guess our relationship was on the rocks in April and May 😳. Surfing was pretty amazing, we both bawsed it up and killed it (pretty sure we both stood up more times than Nikki and caught more metres of surf travelled, suck it dumb hoe). <br><br> Anyway, about the photo at hand... my birthday! This was honestly such a great night, you getting to bond with my mom and spending time with her made me soooo happy. Feasting on a bunch of her cooking (needed salt xD), looking through baby photos (so cliché), you seeing where I grew up (WEIRD!), all very necessary and memorable experiences I will look back on
+Jun 1, 2022 (cont.); My girlfriend with my two other girlfriends 👩‍👩‍👧‍👦😏. Love how great you are at getting along with all my ex lovers, and you and Jiasi are SO cute together. Really random bar but great to see everyone and give them all Covid. Also we were really living it up in that high rise Sonder downtown! The entire Montreal trip was, despite the circumstances, extremely fun. Just getting to spend time with you cooped up in our high rise apartment and play house, our brief illegal and immoral walks in the mountain, and visit to the Botanical Gardens (shortened edition) were all splendid. We're going back soon unhampered by illness and tearing up the whole city with our litness. Also was very cool to merge my life in Montreal with my life with you once again, and the more I do it the more I love seeing it all together 🥰
+Jun 17, 2022; PARISSSSSSSS
+
+
+`;
+
 let parent = document.querySelector('#parent');
 let leftTemplate = document.querySelector('#left-template');
 let rightTemplate = document.querySelector('#right-template');
 let numImages = 50;
+let texts = text.split('\n');
 
 for (let i = 0; i < numImages - 1; i += 2) {
     let l = leftTemplate.cloneNode(true);
@@ -12,12 +45,17 @@ for (let i = 0; i < numImages - 1; i += 2) {
     // Set the image and text.
     l.querySelector('img').src = `images/img${i}.jpg`;
     r.querySelector('img').src = `images/img${i + 1}.jpg`;
-
-    console.log(l.querySelector('img').src);
-    console.log(r.querySelector('img').src);
+    if (texts[i] === undefined || texts[i+1] === undefined) {
+        break;
+    }
+    let [leftDate, leftPar] = texts[i].split(';');
+    let [rightDate, rightPar] = texts[i + 1].split(';');
+    l.querySelector('.box').innerHTML = `<b>${leftDate}</b><br>${leftPar}`;
+    r.querySelector('.box').innerHTML = `<b>${rightDate}</b><br>${rightPar}`;
 
     parent.appendChild(l);
     parent.appendChild(r);
 }
 
 AOS.init();
+
